6108132b852c45db0e8558487eb5f19e32099adb
Jan Temmerman <jantemmerman@gmail.com>
2020-12-08 20:55:42 +0100

    Day 2: Part 1

---
+++ day2/main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strconv"
+	"strings"
+)
+
+type Policy struct {
+	MinCount int
+	MaxCount int
+	Char     byte
+}
+
+type PasswordEntry struct {
+	Policy   Policy
+	Password string
+}
+
+func readEntries(filename string) ([]PasswordEntry, error) {
+	entries := make([]PasswordEntry, 0)
+
+	bytes, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, line := range strings.Split(string(bytes), "\n") {
+		if len(line) == 0 {
+			continue
+		}
+
+		entry, err := readEntry(line)
+		if err != nil {
+			return nil, err
+		}
+
+		entries = append(entries, entry)
+	}
+
+	return entries, nil
+}
+
+func readEntry(line string) (PasswordEntry, error) {
+	lineParts := strings.Split(line, ":")
+	var entry PasswordEntry
+	if len(lineParts) != 2 {
+		return entry, errors.New("entry should be formatted as <policy>: <password>")
+	}
+
+	entry.Password = string(lineParts[1][1:])
+
+	policyParts := strings.Split(lineParts[0], " ")
+	if len(policyParts) != 2 {
+		return entry, errors.New("policy should be <min>-<max> <char>")
+	}
+
+	entry.Policy.Char = policyParts[1][0]
+	policyCounts := strings.Split(policyParts[0], "-")
+	if len(policyCounts) != 2 {
+		return entry, errors.New("policy should be <min>-max> <char>")
+	}
+
+	minCount, err := strconv.ParseInt(policyCounts[0], 10, 32)
+	if err != nil {
+		return entry, err
+	}
+
+	maxCount, err := strconv.ParseInt(policyCounts[1], 10, 32)
+	if err != nil {
+		return entry, err
+	}
+
+	entry.Policy.MinCount = int(minCount)
+	entry.Policy.MaxCount = int(maxCount)
+
+	return entry, nil
+}
+
+func countValidEntries(entries []PasswordEntry) int {
+	count := 0
+	for _, entry := range entries {
+		if entry.IsValid() {
+			count += 1
+		}
+	}
+	return count
+}
+
+func (entry *PasswordEntry) IsValid() bool {
+	charCount := 0
+	for _, b := range entry.Password {
+		if byte(b) == entry.Policy.Char {
+			charCount += 1
+		}
+	}
+
+	return charCount >= entry.Policy.MinCount && charCount <= entry.Policy.MaxCount
+}
+
+func main() {
+	entries, err := readEntries("custom_input.txt")
+	if err != nil {
+		log.Fatalf("Invalid test input!: %s\n", err)
+	}
+
+	fmt.Printf("Valid entries: %d\n", countValidEntries(entries))
+}
